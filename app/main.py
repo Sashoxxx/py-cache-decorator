d1392bdff439e250dea73f6f352f59c05a6a2f3b
Oleksandr Pivtorak <Sanjok150397@gmail.com>
2025-06-20 12:45:55 +0300

    implemented cache deco

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    _cache = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> int:
+        if args in _cache:
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            _cache[args] = func(*args, **kwargs)
+        return _cache[args]
+    return wrapper
